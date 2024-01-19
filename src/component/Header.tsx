4c16203babe0b0f415559d2bc67ef4eb1b5069f9
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-01-19 18:02:25 +0330

    make all component and add some package

---
+++ src/component/Header.tsx
@@ -0,0 +1,31 @@
+function Header() {
+  return (
+    <>
+      <ul>
+        <li>
+          <ul>
+            <li>SHOP ALL</li>
+            <li>PURIFICATION</li>
+            <li>HOME</li>
+            <li>DRINKWARE</li>
+          </ul>
+        </li>
+        <li>LARQ</li>
+        <li>
+          <ul>
+            <li>TECHNOLOGY</li>
+            <li>FAQ</li>
+            <li>
+              <box-icon name="user"></box-icon>
+            </li>
+            <li>
+              <box-icon name="cart"></box-icon>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/declaration.d.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars
+declare namespace JSX {
+  interface IntrinsicElements {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    "box-icon": any;
+  }
+}
