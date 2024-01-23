2b54457e7f0d9dd91fd1da016d2070498e14e6e9
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-01-23 11:22:57 +0330

    add basic of subMenu

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useState } from "react";
+
+const Main = styled.header`
+  display: flex;
+  flex-direction: column;
+`;
 
 const NavBar = styled.ul`
   display: flex;
@@ -32,31 +38,99 @@ const LiAnimated = styled.li`
   }
 `;
 
+const Expanded = styled.div<{ $active?: string }>`
+  /* display: none; */
+  display: ${(props) => props.$active};
+  padding: 0.5vw 2vw;
+  box-sizing: border-box;
+  align-self: self-start;
+  width: 100vw;
+  background-color: #fff;
+  z-index: 10;
+  display: flex;
+  justify-content: space-between;
+  height: 50vh;
+  .links {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    text-align: left;
+  }
+
+  .main-links {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .products {
+    display: flex;
+  }
+`;
+
 function Header() {
+  const [view, setView] = useState("none");
+
   return (
-    <NavBar>
-      <li>
-        <NavBar>
-          <LiAnimated>SHOP ALL</LiAnimated>
-          <LiAnimated>PURIFICATION</LiAnimated>
-          <LiAnimated>HOME</LiAnimated>
-          <LiAnimated>DRINKWARE</LiAnimated>
-        </NavBar>
-      </li>
-      <Li>LARQ</Li>
-      <li>
-        <NavBar>
-          <li>TECHNOLOGY</li>
-          <li>FAQ</li>
-          <li>
-            <box-icon name="user"></box-icon>
-          </li>
-          <li>
-            <box-icon name="cart"></box-icon>
-          </li>
-        </NavBar>
-      </li>
-    </NavBar>
+    <Main onMouseLeave={() => setView("none")}>
+      <NavBar>
+        <li>
+          <NavBar>
+            <LiAnimated onMouseEnter={() => setView("flex")}>
+              SHOP ALL
+            </LiAnimated>
+            <LiAnimated>PURIFICATION</LiAnimated>
+            <LiAnimated>HOME</LiAnimated>
+            <LiAnimated>DRINKWARE</LiAnimated>
+          </NavBar>
+        </li>
+        <Li>LARQ</Li>
+        <li>
+          <NavBar>
+            <li>TECHNOLOGY</li>
+            <li>FAQ</li>
+            <li>
+              <box-icon name="user"></box-icon>
+            </li>
+            <li>
+              <box-icon name="cart"></box-icon>
+            </li>
+          </NavBar>
+        </li>
+      </NavBar>
+      <Expanded $active={view}>
+        <div className="links">
+          <div className="main-links">
+            <a href="#">Explore all</a>
+            <a href="#">Gifts sets</a>
+            <div>Filters & Accessories</div>
+            <div>Corporate gifting</div>
+          </div>
+          <div className="learn-more">
+            <div>LEARN MORE</div>
+            <div>PureVis technology</div>
+            <div>Nano Zero technology</div>
+          </div>
+        </div>
+        <div className="products">
+          <div>
+            <img src="" alt="" />
+            <div>Purification</div>
+            <div>Filter or purify water on the go</div>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>Home</div>
+            <div>Equip your home with Nano Zero filtration</div>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>Drinkware</div>
+            <div>Hydrate effortlessly on the go</div>
+          </div>
+        </div>
+      </Expanded>
+      <div />
+    </Main>
   );
 }
 
